8c224e2a0928f6bbeeeef9544ff3f1291d7eff07
kwiskowski <krzysztof.wiskowski@gmail.com>
2023-08-17 15:24:12 +0200

    set up quiz

--- main.js
+++ main.js
@@ -1,81 +1,102 @@
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male",
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female",
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male",
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female",
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
-    isActive: true,
-    balance: 3951,
-    gender: "male",
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: [
-      "Jacklyn Lucas",
-      "Linda Chapman",
-      "Adrian Cross",
-      "Solomon Fokes",
-    ],
-    isActive: false,
-    balance: 1498,
-    gender: "male",
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female",
-  },
-];
+import { myQuestions } from "./questions.js";
+console.log(myQuestions);
 
-const getTotalBalanceByGender = (users, gender) => {
-  [...users]
-    .flatMap((user) => user.gender)
-    .filter((userGender, index, array) => array.indexOf(userGender) === gender)
-    .reduce((value, user) => {
-      return value + user.balance;
-    }, 0);
-};
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
-console.log(getTotalBalanceByGender(users, "male"));
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(
+  questions,
+  quizContainer,
+  resultsContainer,
+  submitButton
+) {
+  function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
+    const output = [];
+    let answers;
+    let letter = ["a", "b", "c"];
+
+    // for each question...
+    for (let i = 0; i < questions.length; i++) {
+      // first reset the list of answers
+      answers = [];
+
+      // for each available answer...
+      for (letter in questions[i].answers) {
+        // ...add an html radio button
+        answers.push(
+          "<label>" +
+            '<input type="radio" name="question' +
+            i +
+            '" value="' +
+            letter +
+            '">' +
+            letter +
+            ": " +
+            questions[i].answers[letter] +
+            "</label>"
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        '<div class="question">' +
+          questions[i].question +
+          "</div>" +
+          '<div class="answers">' +
+          answers.join("") +
+          "</div>"
+      );
+    }
+
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults(questions, quizContainer, resultsContainer) {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let userAnswer = "";
+    let numCorrect = 0;
+
+    // for each question...
+    for (i = 0; i < questions.length; i++) {
+      // find selected answer
+      userAnswer = (
+        answerContainers[i].querySelector(
+          "input[name=question" + i + "]:checked"
+        ) || {}
+      ).value;
+
+      // if answer is correct
+      if (userAnswer === questions[i].correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[i].style.color = "lightgreen";
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[i].style.color = "red";
+      }
+    }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
+  }
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
+
+  // on submit, show results
+  submitButton.onclick = function () {
+    showResults(questions, quizContainer, resultsContainer);
+  };
+}
---
+++ questions.js
@@ -0,0 +1,74 @@
+export const myQuestions = [
+  {
+    question: "Czym się żywi sarna?",
+    answers: {
+      a: "Jest drapieżnikiem",
+      b: "Roślinami",
+      c: "Jest wszystkożerna",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Jak się nazywa samiec sarny?",
+    answers: {
+      a: "Kozioł",
+      b: "Byk",
+      c: "Saren",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Jak się nazywa kostny twór na głowie samca sarny?",
+    answers: {
+      a: "poroże",
+      b: "rogi",
+      c: "łopaty",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Gdzie odpoczywają sarny?",
+    answers: {
+      a: "w domu",
+      b: "w barłogu",
+      c: "w ostoi",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Kiedy możemy spotkać małe sarny?",
+    answers: {
+      a: "na jesieni",
+      b: "wiosną",
+      c: "na początku zimy",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Co takiego zbiera pszczoła z kwiatów?",
+    answers: {
+      a: "pyłek i nektar",
+      b: "węze",
+      c: "miód",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Co oznacza odcień wosku pszczelego?",
+    answers: {
+      a: "im jaśniejszy tym starszy",
+      b: "im ciemniejszy tym starszy",
+      c: "u każdego gatunku pszczół jest innego koloru",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "W jakich porach roku pszczoły zapylają rośliny?",
+    answers: {
+      a: "wiosną i latem",
+      b: "latem i jesienią",
+      c: "cały rok",
+    },
+    correctAnswer: "a",
+  },
+];
