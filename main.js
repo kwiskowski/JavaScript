25ab189175bbc6ed5191a89d3337e3a02e1d7afc
kwiskowski <krzysztof.wiskowski@gmail.com>
2023-09-21 15:31:35 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,94 @@
-import { myQuestions } from "./questions.js";
+import { questions } from "./questions.js";
 import { congratulations } from "./questions.js";
 import { password } from "./questions.js";
+
+const quizTitle = document.getElementById("quiz-title");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  quizTitle.innerHTML = "Leśna Księga Życia";
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  quizTitle.innerHTML = `Udało Ci się zdobyć ${score} z ${questions.length}!`;
+
+  if (score >= 6) {
+    questionElement.innerHTML = `${congratulations} ${password}`;
+  } else {
+    questionElement.innerHTML = "Prawie!, aby zdobyć hasło spróbuj jeszcze raz";
+  }
+  nextButton.innerHTML = "Zagraj jeszcze raz";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+// startQuiz();
--- questions.js
+++ questions.js
@@ -1,78 +1,87 @@
-export const myQuestions = [
+export const questions = [
+  // {
+  //   question: "PYTANIE?",
+  //   answers: [
+  //     { text: "A", correct: false },
+  //     { text: "B", correct: true },
+  //     { text: "C", correct: false },
+  //   ],
+  // },
+
   {
     question: "Czym się żywi sarna?",
-    answers: {
-      a: "Jest drapieżnikiem",
-      b: "Roślinami",
-      c: "Jest wszystkożerna",
-    },
-    correctAnswer: "b",
+    answers: [
+      { text: "Jest drapieżnikiem", correct: false },
+      { text: "Roślinami", correct: true },
+      { text: "Jest wszystkożerna", correct: false },
+    ],
   },
+
   {
     question: "Jak się nazywa samiec sarny?",
-    answers: {
-      a: "Kozioł",
-      b: "Byk",
-      c: "Saren",
-    },
-    correctAnswer: "a",
+    answers: [
+      { text: "Kozioł", correct: true },
+      { text: "Byk", correct: false },
+      { text: "Saren", correct: false },
+    ],
   },
+
   {
     question: "Jak się nazywa kostny twór na głowie samca sarny?",
-    answers: {
-      a: "poroże",
-      b: "rogi",
-      c: "łopaty",
-    },
-    correctAnswer: "a",
+    answers: [
+      { text: "poroże", correct: true },
+      { text: "rogi", correct: false },
+      { text: "łopaty", correct: false },
+    ],
   },
+
   {
     question: "Gdzie odpoczywają sarny?",
-    answers: {
-      a: "w domu",
-      b: "w barłogu",
-      c: "w ostoi",
-    },
-    correctAnswer: "c",
+    answers: [
+      { text: "w domu", correct: false },
+      { text: "w barłogu", correct: false },
+      { text: "w ostoi", correct: true },
+    ],
   },
+
   {
     question: "Kiedy możemy spotkać małe sarny?",
-    answers: {
-      a: "na jesieni",
-      b: "wiosną",
-      c: "na początku zimy",
-    },
-    correctAnswer: "b",
+    answers: [
+      { text: "na jesieni", correct: false },
+      { text: "wiosną", correct: true },
+      { text: "na początku zimy", correct: false },
+    ],
   },
+
   {
     question: "Co takiego zbiera pszczoła z kwiatów?",
-    answers: {
-      a: "pyłek i nektar",
-      b: "węze",
-      c: "miód",
-    },
-    correctAnswer: "a",
+    answers: [
+      { text: "pyłek i nektar", correct: true },
+      { text: "węze", correct: false },
+      { text: "miód", correct: false },
+    ],
   },
+
   {
     question: "Co oznacza odcień wosku pszczelego?",
-    answers: {
-      a: "im jaśniejszy tym starszy",
-      b: "im ciemniejszy tym starszy",
-      c: "u każdego gatunku pszczół jest innego koloru",
-    },
-    correctAnswer: "b",
+    answers: [
+      { text: "im jaśniejszy tym starszy", correct: false },
+      { text: "im ciemniejszy tym starszy", correct: true },
+      { text: "u każdego gatunku pszczół jest innego koloru", correct: false },
+    ],
   },
+
   {
-    question: "W jakich porach roku pszczoły zapylają rośliny?",
-    answers: {
-      a: "wiosną i latem",
-      b: "latem i jesienią",
-      c: "cały rok",
-    },
-    correctAnswer: "a",
+    question: "w jakich porach roku pszczoły zapylają rośliny?",
+    answers: [
+      { text: "wiosną i latem", correct: true },
+      { text: "latem i jesienią", correct: false },
+      { text: "cały rok", correct: false },
+    ],
   },
 ];
 
 export const congratulations =
   "Gratulacje, udało Ci się przejść Quiz, Twoje tajne hasło, dzięki któremu odbierzesz swoją nagrodę to:";
+
 export const password = "  Leśna Księga Życia";
